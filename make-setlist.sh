9f3ac84b1216ed510c8137946758dde5034d9a76
Philip Johnson <johnson@hawaii.edu>
2023-08-24 09:28:56 -1000

    checkpoint

--- make-setlist.sh
+++ make-setlist.sh
@@ -5,6 +5,8 @@ SETLIST="23-10-15"
 VERSION="1"
 mkdir -p setlists/$SETLIST
 
-perl $CHORDPRO --config=config.json -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST-v${VERSION}.pdf
+perl $CHORDPRO --config=config.json --meta setlist=$SETLIST -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST-v${VERSION}.pdf
 
-perl $CHORDPRO --config=config.json songs/BlazeOn.chopro -o setlists/$SETLIST/BlazeOn.pdf
+perl $CHORDPRO --meta=user=$SETLIST --config=config.json songs/BlazeOn.chopro -o setlists/$SETLIST/BlazeOn.pdf
+
+perl $CHORDPRO --meta=user=$SETLIST -l --config=config.json songs/BlazeOn.chopro -o setlists/$SETLIST/BlazeOn-lyrics.pdf
