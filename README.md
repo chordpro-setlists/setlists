1182f17309df9ea3495bf7cfa530b3dd9ed0d964
Philip Johnson <johnson@hawaii.edu>
2023-08-26 11:19:28 -1000

    updates

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # setlists
 
+# Requirements
+* Provide single source for lyrics/no-lyrics.
+* Provide multisong PDF plus individual songs. 
+* Include key of song and reference version information.
+* Provide historical record of arrangements for future reference.
+
 ## To Do
-* Make chords more visible in formatting for Kevin. (bold, blue, equal sized).
-* Create sample TT setlist to distribute. (lyrics, no lyrics, KH's).
-* Create config file for TOC title specification.
 * Implement a GitHub action to run a gh-pages for the setlists/ directory.
 * Maybe add an index.md with links to make it easier to retrieve.
-* Make charts all start with YYMMDD so they sort nicely.
-* Create a shell script to read the contents of the setlist file and generate individual pages. 
--- make-setlist.sh
+++ make-setlist.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 CHORDPRO=~/github/chordpro/chordpro/script/chordpro
-SETLIST="23-10-15"
+SETLIST="tt-2023-08-19"
 VERSION="1"
 mkdir -p setlists/$SETLIST
+mkdir -p setlists/$SETLIST/songs
 
-perl $CHORDPRO --config=config.json --meta setlist=$SETLIST -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST-v${VERSION}.pdf
+## Create the setlists (with and without lyrics)
+echo "Generating setlists..."
+perl $CHORDPRO --config=config.json --config=$SETLIST.json -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST-v${VERSION}.pdf
 
-perl $CHORDPRO -l --config=config.json --meta setlist=$SETLIST -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST-v${VERSION}-lyrics.pdf
+perl $CHORDPRO -l --config=config.json --config=$SETLIST.json -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST-v${VERSION}-lyrics.pdf
 
-perl $CHORDPRO --meta=user=$SETLIST --config=config.json songs/BlazeOn.chopro -o setlists/$SETLIST/BlazeOn.pdf
+## Create individual songs (with and without lyrics)
+echo "Generating individual songs..."
+while IFS= read -r filename; do
 
-perl $CHORDPRO --meta=user=$SETLIST -l --config=config.json songs/BlazeOn.chopro -o setlists/$SETLIST/BlazeOn-lyrics.pdf
+  perl $CHORDPRO --meta=user=$SETLIST --config=config.json $filename -o setlists/$SETLIST/"${filename%.chopro}.pdf"
+
+  perl $CHORDPRO --meta=user=$SETLIST -l --config=config.json $filename -o setlists/$SETLIST/"${filename%.chopro}-lyrics.pdf"
+
+done < "$SETLIST.txt"
