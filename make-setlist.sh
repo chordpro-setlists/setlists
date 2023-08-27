b170bf9720ceea4468c1370d0bd272b6906b40cc
Philip Johnson <johnson@hawaii.edu>
2023-08-26 14:51:12 -1000

    maybe fix command line

--- make-setlist.sh
+++ make-setlist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CHORDPRO=~/github/chordpro/chordpro/script/chordpro
+# CHORDPRO=~/github/chordpro/chordpro/script/chordpro
 SETLIST="hsj-2023-08-19"
 VERSION="1"
 mkdir -p setlists/$SETLIST
@@ -8,16 +8,16 @@ mkdir -p setlists/$SETLIST/songs
 
 ## Create the setlists (with and without lyrics)
 echo "Generating setlists..."
-perl $CHORDPRO --config=config.json --config=$SETLIST.json -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST-v${VERSION}.pdf
+chordpro --config=config.json --config=$SETLIST.json -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST-v${VERSION}.pdf
 
-perl $CHORDPRO -l --config=config.json --config=$SETLIST.json -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST-v${VERSION}-lyrics.pdf
+chordpro -l --config=config.json --config=$SETLIST.json -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST-v${VERSION}-lyrics.pdf
 
 ## Create individual songs (with and without lyrics)
 echo "Generating individual songs..."
 while IFS= read -r filename; do
 
-  perl $CHORDPRO --meta=user=$SETLIST --config=config.json $filename -o setlists/$SETLIST/"${filename%.chopro}.pdf"
+  chordpro --meta=user=$SETLIST --config=config.json $filename -o setlists/$SETLIST/"${filename%.chopro}.pdf"
 
-  perl $CHORDPRO --meta=user=$SETLIST -l --config=config.json $filename -o setlists/$SETLIST/"${filename%.chopro}-lyrics.pdf"
+  chordpro --meta=user=$SETLIST -l --config=config.json $filename -o setlists/$SETLIST/"${filename%.chopro}-lyrics.pdf"
 
 done < "$SETLIST.txt"
