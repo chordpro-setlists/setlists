5afd030e6f7baaa6e4e5cf5e078f4439ab25f939
Philip Johnson <johnson@hawaii.edu>
2023-08-28 16:30:03 -1000

    Eliminate versions

--- make-setlist.sh
+++ make-setlist.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-SETLIST="tt-2023-08-19"
-VERSION="1"
+SETLIST="hsj-2023-08-19"
+VERSION="" # Could replace with "-V1" if you want versioning.
 mkdir -p setlists/$SETLIST
 mkdir -p setlists/$SETLIST/songs
 
 ## Create the setlists (with and without lyrics)
 echo "Generating setlists..."
-chordpro --config=config.json --config=$SETLIST.json -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST-v${VERSION}.pdf
+chordpro --config=config.json --config=$SETLIST.json -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST${VERSION}.pdf
 
-chordpro -l --config=config.json --config=$SETLIST.json -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST-v${VERSION}.nochords.pdf
+chordpro -l --config=config.json --config=$SETLIST.json -filelist $SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST${VERSION}.nochords.pdf
 
 ## Create individual songs (with and without lyrics)
 echo "Generating individual songs..."
