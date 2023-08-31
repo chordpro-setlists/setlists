1ee05d3b6d1fa30a0a2cf14538060ba60fc4625e
Philip Johnson <johnson@hawaii.edu>
2023-08-31 08:10:55 -1000

    checkpoint

--- make-index-interior.sh
+++ make-index-interior.sh
@@ -66,7 +66,7 @@ find "$directory" -type f -name "*.nochords.pdf" | sort | while read -r file; do
 
     # Write a link for each non-setlist file using Bootstrap classes
     if [[ $filename != setlist* ]]; then
-      echo "  <li class=\"list-group-item\">$filename_no_ext (<a href=\"$filepath_chords\">chords</a>, <a href=\"$filepath_nochords\">no chords</a>)</li>, <a href=\"$filepath_chopro\">chopro</a>)</li>" >> "$html_file_path"
+      echo "  <li class=\"list-group-item\">$filename_no_ext (<a href=\"$filepath_chords\">chords</a>, <a href=\"$filepath_nochords\">no chords</a>, <a href=\"$filepath_chopro\">chopro</a>)</li>" >> "$html_file_path"
     fi
 done
 
