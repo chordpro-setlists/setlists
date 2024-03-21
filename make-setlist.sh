9f0782b3a87b2a23dc945790729ec49aa3066441
Philip Johnson <johnson@hawaii.edu>
2024-03-20 15:23:55 -1000

    After Party V5

--- make-setlist.sh
+++ make-setlist.sh
@@ -8,8 +8,8 @@
 # SETLIST="jam-2023-12-06"
 # SETLIST="song-test"
 SETLIST="afterparty-2023-04-05"
-VERSIONNUM="4"
-VERSIONSUFFIX="-v4"
+VERSIONNUM="5"
+VERSIONSUFFIX="-v5"
 mkdir -p setlists/$SETLIST
 mkdir -p setlists/$SETLIST/songs
 
