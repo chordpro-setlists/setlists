d077a341e8b47c4960ea59ea8cd848fcf4b282dc
Philip Johnson <johnson@hawaii.edu>
2024-05-13 06:49:52 -1000

    revise names

--- make-setlist.sh
+++ make-setlist.sh
@@ -7,7 +7,7 @@
 # SETLIST="jam-2023-11-08"
 # SETLIST="jam-2023-12-06"
 # SETLIST="song-test"
-SETLIST="hsj-2024-06-08"
+SETLIST="2024-06-08-hsj"
 VERSIONNUM="1"
 VERSIONSUFFIX="-v1"
 mkdir -p setlists/$SETLIST
