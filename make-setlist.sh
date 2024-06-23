4b63cee0d10cfed7db5839c735dcea2207215552
Philip Johnson <johnson@hawaii.edu>
2024-06-23 08:34:08 -1000

    Make browser tab display title correctly

--- make-setlist.sh
+++ make-setlist.sh
@@ -22,21 +22,26 @@ fi
 # Because macOS sed is different and will leave this file behind.
 rm -f scripts/contents.json-e
 
+## Set up the config file.
+cp -f scripts/config-template.json scripts/config.json
+sed -i -e "s/@TITLE@/$SETLIST/g" scripts/config.json
+rm -f scripts/config.json-e
+
 
 ## Create the setlists (with and without lyrics)
 echo "Generating setlist for $SETLIST..."
-chordpro --config=scripts/config-new2.json --config=scripts/contents.json -filelist setlist-definitions/$SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST${VERSIONSUFFIX}.pdf
+chordpro --config=scripts/config.json --config=scripts/contents.json -filelist setlist-definitions/$SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST${VERSIONSUFFIX}.pdf
 
-chordpro -l --config=scripts/config-new2.json --config=scripts/contents.json -filelist setlist-definitions/$SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST${VERSIONSUFFIX}.nochords.pdf
+chordpro -l --config=scripts/config.json --config=scripts/contents.json -filelist setlist-definitions/$SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST${VERSIONSUFFIX}.nochords.pdf
 
 ## Create individual songs (with and without lyrics)
 echo "Generating individual songs..."
 while IFS= read -r filename; do
   echo "  $filename"
 
-  chordpro --meta=user=$SETLIST --config=scripts/config-new2.json $filename -o setlists/$SETLIST/"${filename%.chopro}.pdf"
+  chordpro --meta=user=$SETLIST --config=scripts/config.json $filename -o setlists/$SETLIST/"${filename%.chopro}.pdf"
 
-  chordpro --meta=user=$SETLIST -l --config=scripts/config-new2.json $filename -o setlists/$SETLIST/"${filename%.chopro}.nochords.pdf"
+  chordpro --meta=user=$SETLIST -l --config=scripts/config.json $filename -o setlists/$SETLIST/"${filename%.chopro}.nochords.pdf"
 
   cp $filename setlists/$SETLIST/songs/
 
