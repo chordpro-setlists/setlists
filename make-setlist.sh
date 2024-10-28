2450f40862890144cd7cfb60f26d17249181b149
Philip Johnson <johnson@hawaii.edu>
2024-10-28 09:51:19 -1000

    Update misc

--- make-setlist.sh
+++ make-setlist.sh
@@ -6,11 +6,12 @@
 # SETLIST="jam-2023-10-18"
 # SETLIST="jam-2023-11-08"
 # SETLIST="jam-2023-12-06"
-SETLIST="song-test"
+# SETLIST="song-test"
 # SETLIST="2024-06-28-hsj"
 # SETLIST="2024-07-27-jam"
 # SETLIST="2024-10-04-hsj"
 # SETLIST="repertoire"
+SETLIST="misc"
 VERSIONNUM="1"
 VERSIONSUFFIX="-V1"
 mkdir -p setlists/$SETLIST
