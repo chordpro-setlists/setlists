e789df9efe6dc907a57916e029cdf4e3e6078dfc
Philip Johnson <johnson@hawaii.edu>
2024-11-05 15:42:40 -1000

    phish, add cayman review

--- make-setlist.sh
+++ make-setlist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SETLIST="phish"
+SETLIST="phish"
 # SETLIST="jam-2024-01-10"
 # SETLIST="tt-december"
 # SETLIST="jam-2023-10-18"
@@ -11,11 +11,11 @@
 # SETLIST="2024-07-27-jam"
 # SETLIST="2024-10-04-hsj"
 #SETLIST="2024-10-29-hsj"
-SETLIST="2024-12-07-hsj"
+# SETLIST="2024-12-07-hsj"
 # SETLIST="repertoire"
 # SETLIST="misc"
-VERSIONNUM="5"
-VERSIONSUFFIX="-V5"
+VERSIONNUM="1"
+VERSIONSUFFIX="-V1"
 mkdir -p setlists/$SETLIST
 mkdir -p setlists/$SETLIST/songs
 
