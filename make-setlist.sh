2b712b91f70f277b76002ea94f6e8c5ecaab7c04
Philip Johnson <johnson@hawaii.edu>
2024-11-05 15:48:29 -1000

    update

--- make-setlist.sh
+++ make-setlist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SETLIST="phish"
+# SETLIST="phish"
 # SETLIST="jam-2024-01-10"
 # SETLIST="tt-december"
 # SETLIST="jam-2023-10-18"
@@ -10,12 +10,12 @@ SETLIST="phish"
 # SETLIST="2024-06-28-hsj"
 # SETLIST="2024-07-27-jam"
 # SETLIST="2024-10-04-hsj"
-#SETLIST="2024-10-29-hsj"
-# SETLIST="2024-12-07-hsj"
+# SETLIST="2024-10-29-hsj"
+SETLIST="2024-12-07-hsj"
 # SETLIST="repertoire"
 # SETLIST="misc"
-VERSIONNUM="1"
-VERSIONSUFFIX="-V1"
+VERSIONNUM="5"
+VERSIONSUFFIX="-V5"
 mkdir -p setlists/$SETLIST
 mkdir -p setlists/$SETLIST/songs
 
