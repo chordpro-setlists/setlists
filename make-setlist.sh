35005f1cfbd07175dc9d81ee356f7fafe16c6af9
Philip Johnson <johnson@hawaii.edu>
2024-12-22 08:28:52 -1000

    Build the "all" setlist

--- make-setlist.sh
+++ make-setlist.sh
@@ -6,7 +6,7 @@
 # SETLIST="jam-2023-10-18"
 # SETLIST="jam-2023-11-08"
 # SETLIST="jam-2023-12-06"
-SETLIST="song-test"
+# SETLIST="song-test"
 # SETLIST="2024-06-28-hsj"
 # SETLIST="2024-07-27-jam"
 # SETLIST="2024-10-04-hsj"
@@ -14,8 +14,9 @@ SETLIST="song-test"
 # SETLIST="2024-12-07-hsj"
 # SETLIST="repertoire"
 # SETLIST="misc"
-VERSIONNUM=""
-VERSIONSUFFIX=""
+SETLIST="all"
+VERSIONNUM="1"
+VERSIONSUFFIX="-V1"
 mkdir -p setlists/$SETLIST
 mkdir -p setlists/$SETLIST/songs
 
