626ac27752b2ca3c04a241c7b493226cb346365d
Philip Johnson <johnson@hawaii.edu>
2025-06-13 06:50:13 -1000

    Update US Blues

--- make-setlist.sh
+++ make-setlist.sh
@@ -18,7 +18,7 @@ SETLIST="2025-07-04-hsj"
 # SETLIST="repertoire"
 # SETLIST="misc"
 # SETLIST="all"
-VERSIONNUM="5"
+VERSIONNUM="6"
 VERSIONSUFFIX="-V$VERSIONNUM"
 mkdir -p setlists/$SETLIST
 mkdir -p setlists/$SETLIST/songs
