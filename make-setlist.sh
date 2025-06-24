d3810ec1196e22b65e91753b2af5efabeec4bdc8
Philip Johnson <johnson@hawaii.edu>
2025-06-23 14:06:47 -1000

    Added tempos for Sugar Mag and Sunshine Daydream

--- make-setlist.sh
+++ make-setlist.sh
@@ -18,7 +18,7 @@ SETLIST="2025-07-04-hsj"
 # SETLIST="repertoire"
 # SETLIST="misc"
 # SETLIST="all"
-VERSIONNUM="6"
+VERSIONNUM="7"
 VERSIONSUFFIX="-V$VERSIONNUM"
 mkdir -p setlists/$SETLIST
 mkdir -p setlists/$SETLIST/songs
