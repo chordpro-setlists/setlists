1c61793a6f9da8055f2b41a9279a16bab3ac694c
Philip Johnson <johnson@hawaii.edu>
2025-07-19 16:53:54 -1000

    fix crazy finger chords

--- make-setlist.sh
+++ make-setlist.sh
@@ -6,7 +6,7 @@ SETLIST="2025-08-01-hsj"
 # SETLIST="repertoire"
 # SETLIST="misc"
 # SETLIST="all"
-VERSIONNUM="3"
+VERSIONNUM="4"
 VERSIONSUFFIX="-V$VERSIONNUM"
 mkdir -p setlists/$SETLIST
 mkdir -p setlists/$SETLIST/songs
