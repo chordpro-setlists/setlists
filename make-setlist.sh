e7c226746bedfca25bd63d4f0c68a187515806fd
Philip Johnson <johnson@hawaii.edu>
2025-07-31 12:55:41 -1000

    v6

--- make-setlist.sh
+++ make-setlist.sh
@@ -6,7 +6,7 @@ SETLIST="2025-08-01-hsj"
 # SETLIST="repertoire"
 # SETLIST="misc"
 # SETLIST="all"
-VERSIONNUM="5"
+VERSIONNUM="6"
 VERSIONSUFFIX="-V$VERSIONNUM"
 mkdir -p setlists/$SETLIST
 mkdir -p setlists/$SETLIST/songs
