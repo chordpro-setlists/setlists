db776170e19c3da72cf09db6bff53d1327999cb6
philipmjohnson <johnson@hawaii.edu>
2026-03-22 15:52:39 -1000

    Update make-setlist.sh

--- make-setlist.sh
+++ make-setlist.sh
@@ -2,7 +2,7 @@
 
 
 SETLIST="2026-03-27-hsj"
-VERSIONNUM="7"
+VERSIONNUM="8"
 VERSIONSUFFIX="-V$VERSIONNUM"
 mkdir -p setlists/$SETLIST
 mkdir -p setlists/$SETLIST/songs
@@ -24,9 +24,9 @@ rm -f scripts/config.json-e
 
 ## Create the setlists (with and without lyrics)
 echo "Generating setlist for $SETLIST..."
-chordpro --config=scripts/config.json --config=scripts/contents.json -filelist setlist-definitions/$SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST${VERSIONSUFFIX}.pdf
+chordpro --config=scripts/config.json --config=scripts/contents.json -filelist setlist-definitions/$SETLIST.txt -o setlists/$SETLIST/charts-$SETLIST${VERSIONSUFFIX}.pdf
 
-chordpro -l --config=scripts/config.json --config=scripts/contents.json -filelist setlist-definitions/$SETLIST.txt -o setlists/$SETLIST/setlist-$SETLIST${VERSIONSUFFIX}.nochords.pdf
+chordpro -l --config=scripts/config.json --config=scripts/contents.json -filelist setlist-definitions/$SETLIST.txt -o setlists/$SETLIST/charts-$SETLIST${VERSIONSUFFIX}.nochords.pdf
 
 ## Create individual songs (with and without lyrics)
 #echo "Generating individual songs..."
